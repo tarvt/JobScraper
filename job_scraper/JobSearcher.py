60eef9ad606460cc216221ec98158f464d1670e3
taravat monsef <taravatmonsef@yahoo.com.sg>
2025-04-13 12:33:48 +0330

    [modify] jobsearcher and added JobVisionSearcher

--- job_scraper/JobSearcher.py
+++ job_scraper/JobSearcher.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 class JobSearcher:
     def __init__(self, base_url):
         self.base_url = base_url
@@ -32,7 +39,6 @@ class JobinjaSearcher(JobSearcher):
             for job in job_items:
                 title = job.find('h2', class_='o-listView__itemTitle').get_text(strip=True)
                 company = job.find('span', string=lambda x: '|' in x).get_text(strip=True)
-                #location = job.find('span', class_='c-jobListView__metaItem', string=lambda x: 'تهران' in x or 'اصفهان' in x).get_text(strip=True)
                 link = job.find('a', class_='c-jobListView__titleLink')['href']
                 date_posted = job.find('span', class_='c-jobListView__passedDays').get_text(strip=True)
                 jobs.append({
@@ -43,19 +49,62 @@ class JobinjaSearcher(JobSearcher):
                     'date_posted': date_posted,
                 })
                 # Printing the extracted job listings
-                for job in jobs:
-                    print(f"Title: {job['title']}")
-                    print(f"Company: {job['company']}")
-                    #print(f"Location: {job['location']}")
-                    print(f"Link: {job['link']}")
-                    print(f"Posted: {job['date_posted']}")
-                    print('-' * 40)
+            return jobs
         else:
             return None  # Handle errors appropriately
 
 
-class jobvisionSearcher(JobSearcher):
+class JobVisionSearcher:
 
-    def search_jobs(self, keywords, locations=None, categories=None):
-        # Implement the API call and response handling specific to this job board
-        pass
+    BASE_URL = "https://jobvision.ir/jobs"
+
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run headless Chrome
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+
+    def search_jobs(self, search_field, job_type="remote", page=1, sort=1):
+        url = f"{self.BASE_URL}/keyword/{search_field}/type/{job_type}?page={page}&sort={sort}"
+        self.driver.get(url)
+
+
+        try:
+            WebDriverWait(self.driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "div.jobs-list.row.rounded.no-border-radius-left job-card-list job-card"))
+            )
+
+        except Exception as e:
+            print("An unexpected error occurred:", e)
+
+        html = self.driver.page_source
+        return self.parse_job_results(html)
+
+
+
+    def parse_job_results(self, html):
+        soup = BeautifulSoup(html, "html.parser")
+        job_cards = soup.select("div.jobs-list.row.rounded.no-border-radius-left job-card-list job-card")
+        jobs = []
+
+        for job in job_cards:
+            print(job)
+            job_title = soup.select_one('.job-card-title').get_text(strip=True)
+            company_name_element = soup.select_one('.company-brand + a')
+            company_name = company_name_element.get_text(strip=True) if company_name_element else 'N/A'
+            job_link = soup.select_one('a[aria-label][href]')['href']
+            jobs.append({
+                    'title': job_title,
+                    'company': company_name,
+                    #'location': location,
+                    'link': job_link,
+                    #'date_posted': date_posted,
+                })
+
+
+        return jobs 
+
+
+    def close(self):
+
+        self.driver.quit()
